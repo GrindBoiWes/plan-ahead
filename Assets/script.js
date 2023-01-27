efecc819d62ecaca560d28cf57271ffec5935795
Wes Harrison <wesharrison89@gmail.com>
2023-01-26 17:10:23 -0800

    added to JS file

--- Assets/script.js
+++ Assets/script.js
@@ -2,15 +2,41 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-var todaysDate = dayjs().format('dddd, MMM Do YYYY');
+var todaysDate = dayjs().format('dddd, MMMM YYYY');
 $('#currentDay').html(todaysDate);
 
 
 
-$(function () {
- 
-});
+$(document).ready(function () {
+ $('.saveBtn').on('click', function() {
+  var time = $(this).siblings.(".description");
+  var text = $(this).parent().attr("id");
 
+  localStorage.setItem(time,text);
+ })
+
+    function timeofDay() {
+      currentTime = dayjs().hour();
+
+      $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+
+        if (blockTime === currentTime) {
+          $(this).removeClass("past");
+          $(this).removeClass("future");
+          $(this).addClass("present");
+        } else if (blockTime > currentTime) {
+          $(this).removeClass("past");
+          $(this).removeClass("present");
+          $(this).addClass("future");
+        } else if (blockTime < currentTime) {
+          $(this).removeClass("present");
+          $(this).removeCLass("future");
+          $(this).addClass("past");
+        }
+      })
+    }
 
 
 
