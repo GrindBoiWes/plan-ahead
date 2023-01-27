b63313dcd34b82eba7d441076ca06b0a72dc10aa
Wes Harrison <wesharrison89@gmail.com>
2023-01-26 21:34:01 -0800

    added local storage files

--- Assets/script.js
+++ Assets/script.js
@@ -3,19 +3,19 @@
 // in the html.
 
 var todaysDate = dayjs().format('dddd, MMMM YYYY');
-$('#currentDay').html(todaysDate);
+$('#currentDay').text(todaysDate);
 
 
 
 $(document).ready(function () {
  $('.saveBtn').on('click', function() {
-  var time = $(this).siblings.(".description");
+  var time = $(this).siblings(".description").val();
   var text = $(this).parent().attr("id");
 
   localStorage.setItem(time,text);
  })
 
-    function timeofDay() {
+    function timeOfDay() {
       currentTime = dayjs().hour();
 
       $(".time-block").each(function () {
@@ -32,15 +32,30 @@ $(document).ready(function () {
           $(this).addClass("future");
         } else if (blockTime < currentTime) {
           $(this).removeClass("present");
-          $(this).removeCLass("future");
+          $(this).removeClass("future");
           $(this).addClass("past");
         }
       })
     }
 
 
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-console.log(dayjs())
+
+
+
+
+    timeOfDay();
+  });
+
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
